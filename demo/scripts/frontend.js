12280897ff267553a75aaa230819b63876b9c87f
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2024-09-08 02:38:04 +0200

    updated demo

--- demo/scripts/frontend.js
+++ demo/scripts/frontend.js
@@ -17,7 +17,7 @@ function observeAnimatedValues() {
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    animateValue(object, 0, object.textContent, 250);
+                    animateValue(object, 0, object.textContent, 2500);
                     observer.unobserve(entry.target);
                 }
             });
@@ -28,30 +28,33 @@ function observeAnimatedValues() {
 
 function switchToDark() {
     const topBar = document.getElementsByClassName("top-bar")[0];
-    const textButton = document.getElementsByClassName("text")[0];
-    const logoImg = document.getElementsByClassName("logo")[0].getElementsByTagName("img")[0];
-
     topBar.style.backgroundColor = "black";
-    topBar.style.border = "1px solid rgb(30, 30, 30)";
+    topBar.style.border = "1px solid rgba(30, 30, 30)";
 
+    const textButton = document.getElementsByClassName("text")[0];
     textButton.style.backgroundColor = "white";
     textButton.style.color = "black";
 
+    const logoImg = document.getElementsByClassName("logo")[0].getElementsByTagName("img")[0];
     logoImg.src = "assets/logo-dark-mode.png";
+
+    document.getElementById("page-icon").href = "assets/logo-dark-mode.png";
 }
 
 function switchToLight() {
-    const topBar = document.getElementsByClassName("top-bar")[0];
-    const textButton = document.getElementsByClassName("text")[0];
-    const logoImg = document.getElementsByClassName("logo")[0].getElementsByTagName("img")[0];
 
+    const topBar = document.getElementsByClassName("top-bar")[0];
     topBar.style.backgroundColor = "white";
     topBar.style.border = "1px solid rgb(225, 225, 225)";
 
+    const textButton = document.getElementsByClassName("text")[0];
     textButton.style.backgroundColor = "black";
     textButton.style.color = "white";
 
-    logoImg.src = "assets/logo-light-mode.png";
+    const logoImg = document.getElementsByClassName("logo")[0].getElementsByTagName("img")[0];
+    logoImg.src = "assets/logo-extended-light-mode.png";
+
+    document.getElementById("page-icon").href = "assets/logo-light-mode.png";
 }
 
 function handleScroll() {
